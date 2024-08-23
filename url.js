88216e6cefd8dc96b1d5bce99e26aa7f18b317bb
Gautham <gthampies@gmail.com>
2024-08-23 22:45:20 +0530

    fix:url change

--- url.js
+++ url.js
@@ -1,2 +1,2 @@
-export const localhost = "http://localhost:3001"
-// export const localhost = "http://api.thevoyage.live"
+// export const localhost = "http://localhost:3001"
+export const localhost = "http://api.thevoyage.live"

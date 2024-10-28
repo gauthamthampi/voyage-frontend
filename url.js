43e92acfa6b0122b3333dc3730b097aa0943ba21
Gautham <gthampies@gmail.com>
2024-10-28 10:56:30 +0530

    fix:url changes

--- url.js
+++ url.js
@@ -2,5 +2,5 @@
 // export const localhost = "http://api.thevoyage.live"
 // export const localhost = "https://voyage-backend-ogoh2k995-gauthams-projects-f3404a2c.vercel.app"
 // export const localhost = 'https://voyage-backend-f96q.onrender.com'
-// export const localhost = 'http://thevoyage.live'
-export const localhost = 'http://13.51.177.62:3001'
+// export const localhost = 'https://thevoyage.live'
+export const localhost = 'https://13.51.177.62:3001'
